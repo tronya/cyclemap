1ce6f7ae5119a5ef6a817c32939bb3f72ed96b6a
Troniak Yura <tronyakyura@@gmail.com>
2022-06-06 12:54:03 +0300

    Initial Commit

---
+++ whereiam/whereiamApp.swift
@@ -0,0 +1,17 @@
+//
+//  whereiamApp.swift
+//  whereiam
+//
+//  Created by Yurii Troniak on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct whereiamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
